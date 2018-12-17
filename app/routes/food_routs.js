fc459d98774b1285fd857ed2b11a89e7af7fc5c7
WalaaMedhat <nicedree@gmail.com>
2018-12-17 05:30:12 +0200

    create home page

--- app/routes/food_routs.js
+++ app/routes/food_routs.js
@@ -1,5 +1,9 @@
 module.exports = function(app, db) {
   app.get('/', (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.send(new Buffer('<div style="display:flex;flex-direction: column;align-items: center;justify-content: center;height: 100%;"><h2>Hello everyone <a href="https://food-recipes-api.herokuapp.com/recipes">Click here</a> to show the API</h2><h4 style="margin-top:0;">Created By Walaa Mohtaseb</h4></div>'));
+  })
+  app.get('/recipes', (req, res) => {
     db.collection('food_recipes').find({}).toArray(function (err, result) {
         if (err) {
           console.log(err,'err');
